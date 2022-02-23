20ac1d4ffbaa178cce3023c50be4d00a6597dc87
Milton Luiz <Milton Luiz>
2022-02-23 20:06:08 -0300

    atualizações finais do dia 23/02

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
-import { Dashboard } from './src/screens/Dashboard';
+import{ ThemeProvider } from 'styled-components';
 
+import { Dashboard } from './src/screens/Dashboard';
+import theme from './src/global/styles/theme';
 
 
 export default function App() {
-    return <Dashboard/>
-}
+    return (
+        <ThemeProvider theme = {theme}>
+            
+            <Dashboard/>
+            
+        </ThemeProvider>
+        
+    )
+
+    }
 
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Container } from './styles'
+import { Text } from "react-native";
+import { Container, Title } from './styles'
 
 
 export function Dashboard(){
     return (
         <Container>
-            <Text>Você é demais</Text>
+            <Title>Você é demais</Title>
         </Container>
     )
 }
 
-    const styles = StyleSheet.create ({
-        container: {
-            flex: 1,
-            justifyContent: 'center',
-            alignItems: 'center'
-        }
-    })
