9a2cd0c5afcbd9ff89cb027568cff47b57acafbd
Milton Luiz <Milton Luiz>
2022-02-25 18:36:48 -0300

    Add fontes personalizadas

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import theme from "../../global/styles/theme";
 
 export const Container = styled.View`
     
@@ -11,6 +12,7 @@ export const Container = styled.View`
 `;
 
 export const Title = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.bold};
     font-size: 24px;
     font-weight: bold;
     color: ${({ theme }) => theme.colors.title};
