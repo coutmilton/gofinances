1709802c025c77dd9ca0d425e1bc58dd9b22460e
Milton Luiz <Milton Luiz>
2022-03-04 06:12:02 -0300

    Add button Enviar

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import { Input } from "../../components/Forms/Input"
+import { Button } from "../../components/Forms/Button"
 
 import { 
     Container,
     Header,
     Title,
-    Form
+    Form,
+    Fields
  } from "./style";
 
 export function Register() {
@@ -18,12 +20,15 @@ export function Register() {
             </Header>
 
             <Form>
-                <Input 
-                    placeholder="Nome"
-                />
-                <Input 
-                    placeholder="Preço"
-                />
+                <Fields>
+                    <Input 
+                        placeholder="Nome"
+                    />
+                    <Input 
+                        placeholder="Preço"
+                    />
+                </Fields>
+                <Button title = "Enviar"/>
             </Form>
 
         </Container>
